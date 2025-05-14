ddcba5e75352955e3d7a2be8e15f06a0768b765b
Guantai Jp <jpguantai@gmail.com>
2025-05-14 16:47:50 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'car-dealer-cms',
+  description: 'car-dealer-cms',
+  generator: '',
 }
 
 export default function RootLayout({
